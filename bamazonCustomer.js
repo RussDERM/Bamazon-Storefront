06dac1a66c42ec4a6ff016e7a5a9f07675403da2
Russ <russ.dermody@gmail.com>
2019-05-07 10:42:15 -0700

    Node connected to mySQL db successfully

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,21 @@
 // Require all node packages
-
+var mySQL = require('mySQL');
+var inquirer = require('inquirer');
 
 // mySQL object
+var connection = mySQL.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '$@Squatch!46',
+  database: 'illwill',
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('You are connected as id ' + connection.threadId + '\n');
+});
+
+
 
 // Initialize SQL connection
